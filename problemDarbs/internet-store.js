e53959a777bb09e7f1c09036ecd7a2118cad053f
Nezkas <matiss.urmanis3@gmail.com>
2025-11-05 19:22:01 +0200

    did MD

--- problemDarbs/internet-store.js
+++ problemDarbs/internet-store.js
@@ -1,17 +1,36 @@
-const items = [];
+const items = [
+    {
+        nosaukums: "Viedtālrunis",
+        cena: 699.99,
+        kategorija: "Elektronika",
+    },
+    {
+        nosaukums: "Kleita",
+        cena: 49.99,
+        kategorija: "Apģērbs",
+    },
+    {
+        nosaukums: "Klepjdators",
+        cena: 19.99,
+        kategorija: "Elektronika",
+    },
+    {
+        nosaukums: "Grāmata",
+        cena: 14.99,
+        kategorija: "Literatūra",
+    }
+];
 
-let prece = new Object ();
+const Elektronika = items.filter(item => item.kategorija === "Elektronika");
 
-prece.nosaukums = "Dators";
-prece.kategorija = "Elektronika";
-prece.cena = 800;
-prece.daudzums = 10;
+if (Elektronika.length > 0) {
+    console.log("Elektroniskas preces: ");
+    Elektronika.forEach(item => {
+        console.log(`- ${item.nosaukums}: $${item.cena}`);
+    });
+} else {
+    console.log("Nav pieejamu elektronisku preču.");
+}
 
-
-let prece2 = new Object ();
-
-prece2.nosaukums = "Kleita";
-prece2.kategorija = "apģērbs";
-prece2.cena = 20;
-prece2.daudzums = 100;
-
+cena = items.reduce((sum, item) => sum + item.cena, 0);
+console.log(`Kopējā preču cena: $${cena.toFixed(2)}`);
